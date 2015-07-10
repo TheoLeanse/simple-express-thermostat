3444bb83b9848107ae643a5554c17827b409ea95
TheoLeanse <theoleanse@gmail.com>
2015-07-10 21:15:04 +0100

    configures express session in app.js

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 app.set('views', './views'); // path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); // this allows the body to be filled
+
+app.use(session({
+  secret: 'S3CRE7',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/', function(req, res, next) {
   res.render('index', { title: 'Thermostat' });
 });
 
 app.post('/temperature', function(req, res) {
-  console.log(req.body.temperature);
+  req.session.temperature = req.body.temperature;
 });
 
 var server = app.listen(3000, function() {
