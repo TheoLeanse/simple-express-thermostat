ea5bbef283379a70db9851a5048f75fb6a08c090
TheoLeanse <theoleanse@gmail.com>
2015-09-08 16:37:08 +0100

    adds basic test to configure jasmine in Express

---
+++ spec/mainSpec.js
@@ -0,0 +1,9 @@
+beforeEach(function () {
+    thermostat = new Thermostat();
+});
+
+describe("Thermostat", function () {
+    it("can be tested in jasmine", function () {
+        expect(1+1).toEqual(2);
+    })
+});
